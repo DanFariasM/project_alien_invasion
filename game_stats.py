aa0f775c3ae11586769ac491d6dc427844b2460f
DanFariasM <danfariasm@gmail.com>
2022-11-04 23:42:11 -0400

    Ability to save high score on a file for future players.

--- game_stats.py
+++ game_stats.py
@@ -10,11 +10,22 @@ class GameStats:
         self.game_active = False
 
         # High score should never be reset.
-        self.high_score = 0
+        self.retrieve_high_score()
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
         self.score = 0
         self.level = 1
+
+    def retrieve_high_score(self):
+        """Retrieving the all time high score, except for 1st time players.
+        1st time players will start with a 0 high score."""
+        try:
+            filename = "high_score.txt"
+            with open(filename) as file_object:
+                self.high_score = int(file_object.read())
+        except FileNotFoundError:
+            self.high_score = 0
+
 
